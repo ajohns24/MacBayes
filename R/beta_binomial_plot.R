8e9b8bfd5b6a1f6a7130916f938139869f379ba5
Alicia Johnson <ajohns24@users.noreply.github.com>
2017-09-11 12:51:44 -0500

    updated fncns

--- R/beta_binomial_plot.R
+++ R/beta_binomial_plot.R
@@ -4,29 +4,23 @@
 #' @param a,b Shape parameters of the Beta prior (Beta(a,b))
 #' @param x,n Observed number of successes & trials
 #' @param posterior logical; if TRUE, the posterior pdf is drawn
-#' @param summary logical; if TRUE, the prior/posterior mean, median, and variance are printed
 #' @examples
 #' library(ggplot2)
 #' beta_binomial_plot(a=1, b=1)
 #' beta_binomial_plot(a=1, b=1, x=5, n=10, posterior=TRUE)
 #' @export
-beta_binomial_plot <- function(a, b, x=NULL, n=NULL, posterior=FALSE, summary=TRUE, ...){
+beta_binomial_plot <- function(a, b, x=NULL, n=NULL, posterior=FALSE){
     if(posterior==TRUE & (is.null(x) | is.null(n))) stop('Specify your data x and n')
     
     #prior pdf
     gprior <- ggplot(NULL,aes(x=c(0,1))) + 
         stat_function(fun=dbeta, args=list(shape1=a, shape2=b)) +
         stat_function(fun=dbeta, args=list(shape1=a, shape2=b), geom="ribbon", fill="cyan4", alpha=0.5, mapping = aes(ymin=0, ymax=..y..)) + 
-        labs(x="p", y="pdf") + ...
+        labs(x="p", y="pdf")
     
     if(posterior==FALSE){
-        print(gprior)
-        if(summary==TRUE){
-            prior_mean <- a/(a+b)
-            prior_mode <- (a-1)/(a+b-2)
-            prior_var  <- a*b/((a+b)^2*(a+b+1))
-            return(data.frame(model=c("prior"), mean=prior_mean, mode=prior_mode, var=prior_var))
-        }
+        #plot prior
+        gprior
     }
     
     else{
@@ -34,16 +28,8 @@ beta_binomial_plot <- function(a, b, x=NULL, n=NULL, posterior=FALSE, summary=TR
         gpost <- gprior + 
             stat_function(fun=dbeta, args=list(shape1=a+x, shape2=b+n-x)) +
             stat_function(fun=dbeta, args=list(shape1=a+x, shape2=b+n-x), geom="ribbon", fill="gold1", alpha=0.5, mapping = aes(ymin=0, ymax=..y..)) + 
-            geom_vline(xintercept=x/n, color="red")
-        print(gpost)
-        if(summary==TRUE){
-            prior_mean <- a/(a+b)
-            prior_mode <- (a-1)/(a+b-2)
-            prior_var  <- a*b/((a+b)^2*(a+b+1))
-            post_mean  <- (x+a)/(n+a+b)    
-            post_mode <- (x+a-1)/(n+a+b-2)
-            post_var   <- (x+a)*(n-x+b)/((n+a+b)^2*(n+a+b+1))
-            return(data.frame(model=c("prior","posterior"), mean=c(prior_mean,post_mean), mode=c(prior_mode,post_mode), var=c(prior_var,post_var)))
-        }
+            geom_vline(xintercept=x/n, color="red")    
+        #plot posterior
+        gpost
     }
 }
---
+++ R/beta_binomial_summary.R
@@ -0,0 +1,31 @@
+#' Summarize features of a Beta-Binomial model
+#'
+#' Summarize key features of the prior and posterior pdfs for a Beta-Binomial model.
+#' @param a,b Shape parameters of the Beta prior (Beta(a,b))
+#' @param x,n Observed number of successes & trials
+#' @param posterior logical; if TRUE, the posterior pdf is summarized
+#' @examples
+#' library(ggplot2)
+#' beta_binomial_summary(a=1, b=1)
+#' beta_binomial_summary(a=1, b=1, x=5, n=10, posterior=TRUE)
+#' @export
+beta_binomial_summary <- function(a, b, x=NULL, n=NULL, posterior=FALSE){
+    if(posterior==TRUE & (is.null(x) | is.null(n))) stop('Specify your data x and n')
+    
+    if(posterior==FALSE){
+        prior_mean <- a/(a+b)
+        prior_mode <- (a-1)/(a+b-2)
+        prior_var  <- a*b/((a+b)^2*(a+b+1))
+        return(data.frame(model=c("prior"), mean=prior_mean, mode=prior_mode, var=prior_var))
+    }
+    
+    else{
+        prior_mean <- a/(a+b)
+        prior_mode <- (a-1)/(a+b-2)
+        prior_var  <- a*b/((a+b)^2*(a+b+1))
+        post_mean  <- (x+a)/(n+a+b)    
+        post_mode <- (x+a-1)/(n+a+b-2)
+        post_var   <- (x+a)*(n-x+b)/((n+a+b)^2*(n+a+b+1))
+        return(data.frame(model=c("prior","posterior"), mean=c(prior_mean,post_mean), mode=c(prior_mode,post_mode), var=c(prior_var,post_var)))
+    }
+}
