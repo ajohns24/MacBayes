120578fd882ee315907aed6828074114a0d050a3
Alicia Johnson <ajohns24@users.noreply.github.com>
2019-01-28 14:48:41 -0600

    Add files via upload

--- R/beta_binomial_plot.R
+++ R/beta_binomial_plot.R
@@ -5,30 +5,52 @@
 #' @param x,n Observed number of successes & trials, respectively
 #' @examples
 #' library(ggplot2)
-#' beta_binomial_plot(a=1, b=1)
-#' beta_binomial_plot(a=1, b=1, x=5, n=10)
+#' beta_binomial_plot(a=2, b=1)
+#' beta_binomial_plot(a=2, b=1, x=5, n=10)
 #' @export
-beta_binomial_plot <- function(a, b, x=NULL, n=NULL){
+beta_binomial_plot <- function(alpha, beta, x = NULL, n = NULL, prior = TRUE, likelihood = TRUE, posterior = TRUE){
     if(is.null(x) | is.null(n)) warning('To visualize the posterior, specify data x and n')
 
     #prior pdf
-    gprior <- ggplot(NULL,aes(x=c(0,1))) +
-        stat_function(fun=dbeta, args=list(shape1=a, shape2=b)) +
-        stat_function(fun=dbeta, args=list(shape1=a, shape2=b), geom="ribbon", fill="gold1", alpha=0.5, mapping = aes(ymin=0, ymax=..y..)) +
-        labs(x="p", y="pdf")
+    g <- ggplot(NULL, aes(x = c(0,1))) +
+        labs(x = expression(theta), y = "density") +
+        scale_fill_manual("", values = c("prior" = "gold1", "(scaled) likelihood" = "cyan2", "posterior" = "cyan4"))
 
-    if(is.null(x) & is.null(n)){
-        #plot prior
-        gprior
+    if(prior == TRUE){
+        g <- g +
+            stat_function(fun = dbeta, args=list(shape1 = alpha, shape2 = beta)) +
+            stat_function(fun = dbeta, args=list(shape1 = alpha, shape2 = beta),
+                          geom="area", alpha=0.5, aes(fill = "prior"))
+
     }
 
-    else{
-        #posterior pdf
-        gpost <- gprior +
-            stat_function(fun=dbeta, args=list(shape1=a+x, shape2=b+n-x)) +
-            stat_function(fun=dbeta, args=list(shape1=a+x, shape2=b+n-x), geom="ribbon", fill="cyan4", alpha=0.5, mapping = aes(ymin=0, ymax=..y..)) +
-            geom_vline(xintercept=x/n, color="red")
-        #plot posterior
-        gpost
+    if(!is.null(x) & !is.null(n)){
+        # Posterior shape parameters
+        alpha_post <- alpha + x
+        beta_post  <- beta + n - x
+
+        # Scaled likelihood function
+        x_data      <- x
+        like_scaled <- function(x){
+            like_fun    <- function(x){dbinom(x = x_data, size = n, prob = x)}
+            scale_c     <- integrate(like_fun, lower = 0, upper = 1)[[1]]
+            like_fun(x) / scale_c
+        }
     }
+
+    if(!is.null(x) & !is.null(n) & (likelihood != FALSE)){
+        g <- g +
+            stat_function(fun = like_scaled) +
+            stat_function(fun = like_scaled, geom = "area", alpha = 0.5, aes(fill = "(scaled) likelihood"))
+    }
+
+    if(!is.null(x) & !is.null(n) & posterior == TRUE){
+        g <- g +
+            stat_function(fun = dbeta, args = list(shape1 = alpha_post, shape2 = beta_post)) +
+            stat_function(fun = dbeta, args = list(shape1 = alpha_post, shape2 = beta_post),
+                          geom = "area", alpha = 0.5, aes(fill = "posterior"))
+    }
+
+    # Plot
+    g
 }
--- R/poisson_gamma_plot.R
+++ R/poisson_gamma_plot.R
@@ -8,28 +8,48 @@
 #' poisson_gamma_plot(s=2, r=2)
 #' poisson_gamma_plot(s=2, r=2, sum_x=10, n=5)
 #' @export
-poisson_gamma_plot <- function(s, r, sum_x=NULL, n=NULL){
-    if(is.null(sum_x) | is.null(n)) warning('To visualize the posterior, specify data sum_x and n')
-    xmin <- min(qgamma(0.00001,s,r), qgamma(0.00001,s+sum_x,r+n))
-    xmax <- max(qgamma(0.99999,s,r), qgamma(0.99999,s+sum_x,r+n))
+poisson_gamma_plot <- function(s, r, sum_x = NULL, n = NULL, prior = TRUE, likelihood = TRUE, posterior = TRUE){
+    if(is.null(sum_x) | is.null(n)) warning('To visualize the posterior, specify information about the data: sum_x and n')
+
+    x_min <- min(qgamma(0.00001,s,r), qgamma(0.00001,s+sum_x,r+n))
+    x_max <- max(qgamma(0.99999,s,r), qgamma(0.99999,s+sum_x,r+n))
+
     #prior pdf
-    gprior <- ggplot(NULL,aes(x=c(xmin,xmax))) +
-        stat_function(fun=dgamma, args=list(shape=s, rate=r)) +
-        stat_function(fun=dgamma, args=list(shape=s, rate=r), geom="ribbon", fill="gold1", alpha=0.5, mapping = aes(ymin=0, ymax=..y..)) +
-        labs(x=expression(lambda), y="pdf")
+    g <- ggplot(NULL, aes(x = c(x_min, x_max))) +
+        labs(x = expression(lambda), y = "density") +
+        scale_fill_manual("", values = c("prior" = "gold1", "(scaled) likelihood" = "cyan2", "posterior" = "cyan4"))
 
-    if(is.null(sum_x) & is.null(n)){
-        #plot prior
-        gprior
+    if(prior == TRUE){
+        g <- g +
+            stat_function(fun=dgamma, args=list(shape=s, rate=r)) +
+            stat_function(fun=dgamma, args=list(shape=s, rate=r),
+                          geom="area", alpha=0.5, aes(fill = "prior"))
     }
 
-    else{
-        #posterior pdf
-        gpost <- gprior +
-            stat_function(fun=dgamma, args=list(shape=s+sum_x, rate=r+n)) +
-            stat_function(fun=dgamma, args=list(shape=s+sum_x, rate=r+n), geom="ribbon", fill="cyan4", alpha=0.5, mapping = aes(ymin=0, ymax=..y..)) +
-            geom_vline(xintercept=sum_x/n, color="red")
-        #plot posterior
-        gpost
+    if(!is.null(sum_x) & !is.null(n)){
+        # Posterior shape parameters
+        shape_post <- s + sum_x
+        rate_post  <- r + n
+
+        # Scaled likelihood function
+        like_scaled <- function(x){
+            dgamma(x, shape = sum_x + 1, rate = n)
+        }
     }
+
+    if(!is.null(sum_x) & !is.null(n) & (likelihood != FALSE)){
+        g <- g +
+            stat_function(fun = like_scaled) +
+            stat_function(fun = like_scaled, geom = "area", alpha = 0.5, aes(fill = "(scaled) likelihood"))
+    }
+
+    if(!is.null(sum_x) & !is.null(n) & posterior == TRUE){
+        g <- g +
+            stat_function(fun = dgamma, args = list(shape = shape_post, rate = rate_post)) +
+            stat_function(fun = dgamma, args = list(shape = shape_post, rate = rate_post),
+                          geom = "area", alpha = 0.5, aes(fill = "posterior"))
+    }
+
+    # Plot
+    g
 }
